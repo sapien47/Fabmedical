50145dd219daa41005f06a73339db2f860b245ba
sapien47 <sapien46@gmail.com>
2022-05-18 20:05:51 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,11 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("ed074c2b-64c8-4137-b7ca-d8b4a23a890f");
+appInsights.start()
+
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
